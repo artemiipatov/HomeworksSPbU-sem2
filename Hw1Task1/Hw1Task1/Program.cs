a80d8cb0bf845a92ec3b54dfdf14d6a9774b24ed
ArtiomPatov <patov.988@gmail.com>
2022-02-19 20:38:34 +0300

    fixed mistakes

--- Hw1Task1/Hw1Task1/Program.cs
+++ Hw1Task1/Hw1Task1/Program.cs
@@ -1,29 +1,24 @@
 ﻿using System;
 
-Console.WriteLine("Input array (30 integers limit): ");
+Console.WriteLine("Input array: ");
 string[] strings = Console.ReadLine().Split();
 int[] ints = Array.ConvertAll(strings, int.Parse);
 
-InsertionSort.Sort(ints);
+Sort(ints);
 foreach(int number in ints)
 {
     Console.WriteLine(number);
 }
 
-class InsertionSort
+static void Sort(int[] array)
 {
-    public static void Sort(int[] array)
+    for (int index = 1; index < array.Length; index++)
     {
-        for (int index = 1; index < array.Length; index++)
+        int i = index;
+        while (i > 0 && array[i] < array[i - 1])
         {
-            int i = index;
-            while (i > 0 && array[i] < array[i - 1])
-            {
-                int temp = array[i - 1];
-                array[i - 1] = array[i];
-                array[i] = temp;
-                --i;
-            }
+            (array[i - 1], array[i]) = (array[i], array[i - 1]);
+            --i;
         }
     }
 }
