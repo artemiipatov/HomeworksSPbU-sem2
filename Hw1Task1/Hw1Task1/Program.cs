09c7fdd964f34ea2bda2f357434a291eeee6ed8c
ArtiomPatov <patov.988@gmail.com>
2022-02-19 20:01:39 +0300

    added first version of c# insertion sort implementation

---
+++ Hw1Task1/Hw1Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+Console.WriteLine("Input array (30 integers limit): ");
+string[] strings = Console.ReadLine().Split();
+int[] ints = Array.ConvertAll(strings, int.Parse);
+
+InsertionSort.Sort(ints);
+foreach(int number in ints)
+{
+    Console.WriteLine(number);
+}
+
+class InsertionSort
+{
+    public static void Sort(int[] array)
+    {
+        for (int index = 1; index < array.Length; index++)
+        {
+            int i = index;
+            while (i > 0 && array[i] < array[i - 1])
+            {
+                int temp = array[i - 1];
+                array[i - 1] = array[i];
+                array[i] = temp;
+                --i;
+            }
+        }
+    }
+}
