0ed9c724c568ec7dea9e8aa5e0980c56e724ad0b
ArtiomPatov <patov.988@gmail.com>
2022-02-26 00:20:52 +0300

    added small changes

--- Hw1Task2/Hw1Task2/main.cs
+++ Hw1Task2/Hw1Task2/main.cs
@@ -1,7 +1,8 @@
 ﻿using Bwt;
 
 Console.WriteLine("Input string: ");
+Console.SetCursorPosition(14, 0);
 string str = Console.ReadLine();
 (string, int) result = Transformation.Bwt(str);
-Console.WriteLine(result.Item1);
-Console.WriteLine($"{Transformation.BwtInverse(result.Item1, result.Item2)}");
+Console.WriteLine($"Transformed: {result.Item1}");
+Console.WriteLine($"Original string: {Transformation.BwtInverse(result.Item1, result.Item2)}");
