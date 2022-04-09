f0bc28f88d7ab068c7d6a7ffeaaabcc8e41b47eb
artemiipatov <patov.988@gmail.com>
2022-04-09 19:18:55 +0300

    fixed tests

--- Hw2Task1/TrieTests/TrieTests.cs
+++ Hw2Task1/TrieTests/TrieTests.cs
@@ -8,7 +8,8 @@ namespace TrieTests;
 /// </summary>
 public class Tests
 {
-    private Trie testTrie = new Trie();
+    private Trie testTrie = new();
+    
     [SetUp]
     public void Setup()
     {
@@ -22,11 +23,16 @@ public class Tests
         testTrie.AddItem("testtest");
         testTrie.AddItem("test123");
         testTrie.Remove("test");
-        Assert.IsTrue(testTrie.Contains("testtest") && testTrie.Contains("test123") && !testTrie.Contains("test"));
+        Assert.IsTrue(testTrie.Contains("testtest"));
+        Assert.IsTrue(testTrie.Contains("test123"));
+        Assert.IsFalse(testTrie.Contains("test"));
         testTrie.Remove("testtest");
         Assert.IsTrue(testTrie.Contains("test123"));
         testTrie.Remove("test123");
-        Assert.IsFalse(testTrie.Contains("test") || testTrie.Contains("testtest") || testTrie.Contains("test123"));
+        Assert.IsFalse(testTrie.Contains("test"));
+        Assert.IsFalse(testTrie.Contains("testtest"));
+        Assert.IsFalse(testTrie.Contains("test123"));
+
     }
 
     [Test]
