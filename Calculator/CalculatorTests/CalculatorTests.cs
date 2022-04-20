d0353c07d487e327eca169affb27bacc88d78764
ArtiomPatov <patov.988@gmail.com>
2022-04-21 00:00:22 +0300

    Initial commit

---
+++ Calculator/CalculatorTests/CalculatorTests.cs
@@ -0,0 +1,55 @@
+namespace CalculatorTests;
+
+using NUnit.Framework;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    // +, -, *, /
+    public void TestBinaryOperators()
+    {
+        Assert.Pass();
+    }
+
+    [Test]
+    // sqrt, plus/minus
+    public void TestUnaryOperators()
+    {
+
+    }
+
+    [Test]
+    public void TestClearAndBackspace()
+    {
+
+    }
+
+    [Test]
+    public void TestDoubleOperandInput()
+    {
+
+    }
+
+    [Test]
+    public void TestDoubleDecimalPointInput()
+    {
+
+    }
+
+    [Test]
+    public void TestUsingBackspaceOnEmptyExpression()
+    {
+
+    }
+
+    [Test]
+    public void TestDoubleNegation()
+    {
+
+    }
+}
