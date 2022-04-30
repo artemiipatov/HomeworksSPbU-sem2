cbc6762968574157d10fbe040c4b6c259982ad38
artemiipatov <patov.988@gmail.com>
2022-04-30 23:45:11 +0300

    reworked methods

--- SkipList/SkipList/SkipList.cs
+++ SkipList/SkipList/SkipList.cs
@@ -1,4 +1,6 @@
-﻿namespace SkipList;
+﻿using System.Diagnostics.Metrics;
+
+namespace SkipList;
 
 using System.Collections;
 
@@ -6,30 +8,23 @@ public class SkipList<T> : IList<T> where T : IComparable<T>
 {
     private class SkipListElement
     {
-        /// <summary>
-        /// Node is infinite if value is null
-        /// </summary>
-        public T? Value { get; private set; }
-        public SkipListElement? Next { get; set; }
-        public SkipListElement? Down { get; set; }
+        public SkipListElement?[] Next = new SkipListElement?[5];
 
-        public SkipListElement(T? value)
+        public int NumberOfLevels = 0;
+
+        public T? Value { get; private set; }
+        
+        public SkipListElement(T? value, int numberOfLevels)
         {
             Value = value;
+            NumberOfLevels = numberOfLevels;
         }
     }
 
-    // private IComparer<T> Comparer;
-    private int _numberOfLevels = 1;
-    private SkipListElement? _minLevel;
-    private SkipListElement? _maxLevel;
+    private SkipListElement _firstElement = new SkipListElement(default, 1);
 
-    public SkipList(/*IComparer<T> comparer*/)
+    public SkipList()
     {
-        _minLevel = new SkipListElement(default) {Down = null, Next = null};
-        _maxLevel = _minLevel;
-        Count = 0;
-        // Comparer = comparer;
     }
 
     public IEnumerator<T> GetEnumerator()
@@ -41,190 +36,133 @@ public class SkipList<T> : IList<T> where T : IComparable<T>
     {
         return GetEnumerator();
     }
-    
+
     public void Add(T item)
     {
-        ++Count;
-        if (_minLevel!.Next == null)
+        if (_firstElement.Next[0] == null)
         {
-            _minLevel.Next = new SkipListElement(item);
+            Random rand = new Random();
+            _firstElement.Next[0] = new SkipListElement(item, 1);
             return;
         }
-
-        bool levelUp;
-        AddRecursive(item, _maxLevel!, out levelUp);
-        
-        if (Math.Log2(Count) >= _numberOfLevels && _maxLevel!.Down is not {Next: null})
-        {
-            var newLevel = new SkipListElement(default) {Down = _maxLevel, Next = null};
-            _maxLevel = newLevel;
-            ++_numberOfLevels;
-        }
+        AddRecursive(item, _firstElement, _firstElement.NumberOfLevels);
     }
 
-    private SkipListElement? AddRecursive(T item, SkipListElement currentElement, out bool levelUp)
+    private SkipListElement AddRecursive(T item, SkipListElement currentElement, int currentLevel)
     {
-        while (currentElement.Next != null && item.CompareTo(currentElement.Next.Value) >= 0)
+        while (currentElement.Next[currentLevel - 1] != null && currentElement.Next[currentLevel - 1]!.Value!.CompareTo(item) > 0)
         {
-            currentElement = currentElement.Next;
+            currentElement = currentElement.Next[currentLevel - 1]!;
         }
 
-        Random rand = new Random();
-        
-        if (currentElement.Down == null)
+        if (currentLevel == 1)
         {
-            var newElement = new SkipListElement(item) {Down = null, Next = currentElement.Next};
-            currentElement.Next = newElement;
-            // Дошел до первого уровня. Элемент вставлен. Теперь нужно вставить (если повезет) элементы в предыдущие уровни.
-            if (rand.Next(2) == 1) // Выпала единица -- вставка на верхний уровень.
-            {
-                levelUp = true;
-                return newElement;
-            }
-
-            levelUp = false;
+            int level = new Random().Next(1, _firstElement.NumberOfLevels);
+            var newElement = new SkipListElement(item, level);
+            newElement.Next[0] = currentElement.Next[0];
+            currentElement.Next[0] = newElement;
             return newElement;
         }
 
-        
-        var bottomElement = AddRecursive(item, currentElement.Down, out levelUp);
-        if (levelUp)
+        var addedElement = AddRecursive(item, currentElement, currentLevel - 1);
+        if (currentLevel <= addedElement.NumberOfLevels)
         {
-            var newElement = new SkipListElement(item) {Down = bottomElement, Next = currentElement.Next};
-            currentElement.Next = newElement;
+            addedElement.Next[currentLevel - 1] = currentElement.Next[currentLevel - 1];
+            currentElement.Next[currentLevel - 1] = addedElement.Next[currentLevel - 1];
         }
-        else
-        {
-            return null;
-        }
-        levelUp = rand.Next(2) == 1;
-        return currentElement.Next;
+        return addedElement;
     }
 
     public void Clear()
     {
-        _minLevel = new SkipListElement(default);
-        _maxLevel = _minLevel;
-        Count = 0;
+        _firstElement = new SkipListElement(default, 1);
     }
 
     public bool Contains(T item)
     {
-        if (_minLevel!.Next == null)
+        return ContainsRecursive(item, _firstElement, _firstElement.NumberOfLevels);
+    }
+
+    private bool ContainsRecursive(T item, SkipListElement currentElement, int currentLevel)
+    {
+        while (currentElement.Next[currentLevel - 1] != null && currentElement.Next[currentLevel - 1]!.Value!.CompareTo(item) > 0)
         {
-            return false;
+            currentElement = currentElement.Next[currentLevel - 1]!;
         }
-        var currentElement = _maxLevel;
-        while (true)
-        {
-            while (currentElement!.Next != null && item.CompareTo(currentElement.Next.Value) > 0)
-            {
-                currentElement = currentElement.Next;
-            }
 
-            if (currentElement.Next != null && item.CompareTo(currentElement.Next.Value) == 0)
-            {
-                return true;
-            }
-
-            if (currentElement.Down == null)
-            {
-                return false;
-            }
-
-            currentElement = currentElement.Down;
-        }
+        return currentElement.Next[currentLevel - 1] != null &&
+               currentElement.Next[currentLevel - 1]!.Value!.CompareTo(item) == 0 ||
+               currentLevel != 1 && ContainsRecursive(item, currentElement, currentLevel - 1);
     }
     
     public void CopyTo(T[] array, int arrayIndex)
     {
-        if (_minLevel!.Next == null)
-        {
-            throw new ArgumentNullException();
-        }
-
-        if (arrayIndex >= Count || arrayIndex < 0)
-        {
-            throw new ArgumentOutOfRangeException();
-        }
+        var currentElement = _firstElement.Next[0];
         int counter = 0;
-        var currentElement = _minLevel.Next;
-        while (counter < arrayIndex)
+        while (currentElement != null)
         {
-            currentElement = currentElement!.Next;
+            array[counter] = currentElement.Value!; // null-ом быть не может, так как метод Add добавляет значения с типом T
+            currentElement = currentElement.Next[0];
             counter++;
-        }
-
-        for (counter = 0; currentElement != null; counter++)
-        {
-            array[counter] = currentElement.Value!; // Не может быть null-ом, потому что добавить можно только значения типа T, а не T?
-            currentElement = currentElement.Next;
         }
     }
 
     public bool Remove(T item)
     {
-        if (_minLevel!.Next == null)
+        if (_firstElement.Next[0] == null)
         {
             return false;
         }
-        var isRemoved = RemoveRecursive(item, _maxLevel!);
-        if (!isRemoved) return false;
-        --Count;
-        var currentLevel = _maxLevel;
-        while (currentLevel!.Down?.Next == null)
-        {
-            _maxLevel = _maxLevel!.Down;
-            currentLevel = _maxLevel;
-            --_numberOfLevels;
-        }
-        return isRemoved;
+
+        return RemoveRecursive(item, _firstElement, _firstElement.NumberOfLevels) != -1;
     }
 
-    private bool RemoveRecursive(T item, SkipListElement currentElement)
+    private int RemoveRecursive(T item, SkipListElement currentElement, int currentLevel)
     {
-        while (currentElement.Next != null && item.CompareTo(currentElement.Next.Value) > 0)
+        while (currentElement.Next[currentLevel - 1] != null && currentElement.Next[currentLevel - 1]!.Value!.CompareTo(item) > 0)
         {
-            currentElement = currentElement.Next;
+            currentElement = currentElement.Next[currentLevel - 1]!;
         }
 
-        if (currentElement.Down == null)
+        if (currentLevel == 1)
         {
-            if (item.CompareTo(currentElement.Next!.Value) == 0)
+            if (currentElement.Next[1] == null)
             {
-                currentElement.Next = currentElement.Next.Next;
-                return true;
+                return -1;
             }
-
-            return false;
-        }
-
-        if (RemoveRecursive(item, currentElement.Down!))
-        {
-            if (currentElement.Next != null && item.CompareTo(currentElement.Next!.Value) == 0)
+            if (currentElement.Next[currentLevel - 1]!.Value!.CompareTo(item) == 0)
             {
-                currentElement.Next = currentElement.Next.Next;
+                int level = currentElement.Next[1]!.NumberOfLevels;
+                return level;
             }
-
-            return true;
         }
 
-        return false;
+        int levelOfRemovedElement = RemoveRecursive(item, currentElement, currentLevel - 1);
+        if (levelOfRemovedElement == -1)
+        {
+            return -1;
+        }
+
+        if (currentLevel >= levelOfRemovedElement)
+        {
+            currentElement.Next[currentLevel - 1] = currentElement.Next[currentLevel - 1]!.Next[currentLevel - 1];
+        }
+
+        return levelOfRemovedElement;
     }
 
     public int Count { get; private set; }
     public bool IsReadOnly { get; }
     public int IndexOf(T item)
     {
+        var currentElement = _firstElement.Next[0];
         int counter = 0;
-        var currentElement = _minLevel!.Next;
-        while (currentElement != null && !currentElement.Value!.Equals(item))
+        while (currentElement != null && currentElement.Value!.CompareTo(item) > 0)
         {
-            currentElement = currentElement.Next;
-            ++counter;
+            currentElement = currentElement.Next[0];
         }
 
-        if (currentElement != null && currentElement.Value!.Equals(item))
+        if (currentElement != null && currentElement!.Value!.CompareTo(item) == 0)
         {
             return counter;
         }
@@ -239,63 +177,7 @@ public class SkipList<T> : IList<T> where T : IComparable<T>
 
     public void RemoveAt(int index)
     {
-        if (index < 0 || index >= Count)
-        {
-            throw new ArgumentOutOfRangeException();
-        }
-        if (IsReadOnly)
-        {
-            throw new NotSupportedException();
-        }
-        
-        int counter = 0;
-        var currentElement = _minLevel!.Next;
-        while (counter < index)
-        {
-            currentElement = currentElement!.Next;
-            counter++;
-        }
-
-        // Если в списке есть элементы с одинаковыми значениями, то удаляться будет самый крайний
-        var value = currentElement!.Value;
-        currentElement = _maxLevel!.Next;
-        var previousElement = _maxLevel;
-        if (currentElement == null) // Исправить ошибку здесь. Ошибка: может быть такое, что искомое значение меньше, чем current.next.value
-        {
-            currentElement = _maxLevel.Down!.Next;
-            previousElement = _maxLevel.Down;
-        }
-        RemoveAtRecursive(value!, currentElement!, previousElement);
-    }
-
-    private SkipListElement? RemoveAtRecursive(T item, SkipListElement currentElement, SkipListElement previousElement)
-    {
-        if (!(currentElement.Value!.Equals(item) && (currentElement.Next == null || !currentElement.Next.Value!.Equals(item))))
-        {
-            while (previousElement!.Next != currentElement)
-            {
-                previousElement = previousElement.Next!;
-            }
-            while (currentElement!.Next != null && currentElement.Value!.CompareTo(currentElement.Next.Value) >= 0)
-            {
-                currentElement = currentElement.Next;
-                previousElement = previousElement.Next!;
-            }
-        }
-        
-        if (currentElement.Down == null)
-        {
-            previousElement!.Next = currentElement.Next;
-            return currentElement;
-        }
-
-        if (RemoveAtRecursive(item, currentElement.Down, previousElement.Down!) == currentElement.Down)
-        {
-            previousElement.Next = currentElement.Next;
-            return currentElement;
-        }
-
-        return null;
+        throw new NotImplementedException();
     }
 
     public T this[int index]
