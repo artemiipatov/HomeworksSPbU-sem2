8a6541a3108184603e5491bff25788e37f74a2ec
ArtiomPatov <patov.988@gmail.com>
2022-06-30 15:58:47 +0300

    refactoring

--- StackCalculator/StackCalculator.Tests/StackCalculatorTests.cs
+++ StackCalculator/StackCalculator.Tests/StackCalculatorTests.cs
@@ -20,7 +20,7 @@ public class StackCalculatorTests
     {
         Assert.AreEqual(59.5, StackCalculator.Evaluate("100 2 + 51 / 4 / 59 +", stack));
         Assert.AreEqual(0.25, StackCalculator.Evaluate("32 2 / 2 / 2 / 2 / 2 / 2 / 2 /", stack));
-        Assert.AreEqual(157.5,StackCalculator.Evaluate("219 3 / 33 - 20 + 3 * 8 / 7 *", stack));
+        Assert.AreEqual(157.5, StackCalculator.Evaluate("219 3 / 33 - 20 + 3 * 8 / 7 *", stack));
         Assert.AreEqual(-33965.261, StackCalculator.Evaluate("55 33 913 1030004 + 1000 / * -", stack));
         Assert.AreEqual(1000, StackCalculator.Evaluate("1000", stack));
     }
